d0600cccd50de7376f798551c9635a59a765aba1
Oscar Nicolas Berrocal Hernandez <oscar.berrocalh@campusucc.edu.co>
2021-10-28 09:47:48 -0500

    Se particiono el calcular.cpp en los programas calcular_parte1.cpp y calcular_parte2.cpp

--- calcular_parte1.cpp
+++ calcular_parte1.cpp
@@ -84,10 +84,6 @@ int main() {
 
 	cout << "La area es => " << vf_area << endl;
 
-	//Archivo funcion
-	//vf_area calcular la raiz cuadrada en una variable y imprimir
 
-	vd_raiz = sqrt(vf_area);
-	cout << "La raiz cuadrada de " << vf_area << " es " << vd_raiz;
-	return 0;
-}
+
+
---
+++ calcular_parte2.cpp
@@ -0,0 +1,7 @@
+	//Archivo funcion
+	//vf_area calcular la raiz cuadrada en una variable y imprimir
+
+	vd_raiz = sqrt(vf_area);
+	cout << "La raiz cuadrada de " << vf_area << " es " << vd_raiz;
+	return 0;
+}
